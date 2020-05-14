3fb7761777c712820acd7003cadf23bcc85917a8
cameron-mccawley <cameronismccawley@gmail.com>
2020-05-14 15:40:09 -0700

    started on monster object

---
+++ index.js
@@ -0,0 +1,31 @@
+var monsterData = {
+    name: "Monster",
+    size: "medium",
+    type: "humanoid",
+    tag: "",
+    alignment: "neutral",
+
+    speed: 30,
+    burrowSpeed: 0,
+    climbSpeed: 0,
+    flySpeed: 0,
+    swimSpeed: 0,
+
+    strPoints: 10,
+    dexPoints: 10,
+    conPoints: 10,
+    intPoints: 10,
+    wisPoints: 10,
+    chaPoints: 10,
+
+    cr: 1,
+
+    blindsight: 0,
+    blind: false,
+    darkvision: 0,
+    tremorsense: 0,
+    truesight: 0,
+    telepathy: 0,
+    
+    hpText: "4 (1d8)"
+};
